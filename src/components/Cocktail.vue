c903e38109d3f3f399b5de7c1e0c1c3dd3f2cc7d
azeke <a.smagulov1@unis.kz>
2024-03-24 17:02:53 +0500

    forgot to show ingredients

--- src/components/Cocktail.vue
+++ src/components/Cocktail.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import type { cocktail } from '@/model/cocktail';
+import { useCocktailStore } from '@/stores/cocktailStore';
 const { cocktail } = defineProps<{ cocktail: cocktail }>()
 </script>
 
@@ -10,4 +11,6 @@ const { cocktail } = defineProps<{ cocktail: cocktail }>()
             <span v-for="text in [cocktail.strCategory, cocktail.strAlcoholic, cocktail.strGlass]"
                 class="after:content-['_']">{{ text }}</span>
         </div>
+        <div class="mt-4 text-sm">List of ingredients:</div>
+        <div v-for="{name,measure} in cocktail.ingredients"><span class="font-bold">{{ name }}</span> {{measure}}</div>
 </template>
--- src/model/cocktail.ts
+++ src/model/cocktail.ts
@@ -1,73 +1,77 @@
-export const listOfCocktails=['Margarita','Mojito','A1','Kir'] as const;
+export const listOfCocktails = ["Margarita", "Mojito", "A1", "Kir"] as const;
 
-export type cocktailType=typeof listOfCocktails[number];
+export type cocktailType = (typeof listOfCocktails)[number];
 
 interface cocktailBase {
-    strDrink:cocktailType;
-    strCategory:string;
-    strAlcoholic: string;
-    strGlass: string;
-    strDrinkThumb:string;
+  strDrink: cocktailType;
+  strCategory: string;
+  strAlcoholic: string;
+  strGlass: string;
+  strDrinkThumb: string;
 }
 
 export interface searchResultApi {
-    drinks: cocktailApi[];
+  drinks: cocktailApi[];
 }
 
-export const ingredientMinIndex=1;
-export const ingredientMaxIndex=15;
+export const ingredientMinIndex = 1;
+export const ingredientMaxIndex = 15;
 
 export interface cocktailApi extends cocktailBase {
-    strIngredient1: string;
-    strIngredient2: string;
-    strIngredient3: string;
-    strIngredient4: string;
-    strIngredient5: string;
-    strIngredient6: string;
-    strIngredient7: string;
-    strIngredient8: string;
-    strIngredient9: string;
-    strIngredient10: string;
-    strIngredient11: string;
-    strIngredient12: string;
-    strIngredient13: string;
-    strIngredient14: string;
-    strIngredient15: string;
+  strIngredient1: string;
+  strIngredient2: string;
+  strIngredient3: string;
+  strIngredient4: string;
+  strIngredient5: string;
+  strIngredient6: string;
+  strIngredient7: string;
+  strIngredient8: string;
+  strIngredient9: string;
+  strIngredient10: string;
+  strIngredient11: string;
+  strIngredient12: string;
+  strIngredient13: string;
+  strIngredient14: string;
+  strIngredient15: string;
 
-    strMeasure1: string;
-    strMeasure2: string;
-    strMeasure3: string;
-    strMeasure4: string;
-    strMeasure5: string;
-    strMeasure6: string;
-    strMeasure7: string;
-    strMeasure8: string;
-    strMeasure9: string;
-    strMeasure10: string;
-    strMeasure11: string;
-    strMeasure12: string;
-    strMeasure13: string;
-    strMeasure14: string;
-    strMeasure15: string;
+  strMeasure1: string;
+  strMeasure2: string;
+  strMeasure3: string;
+  strMeasure4: string;
+  strMeasure5: string;
+  strMeasure6: string;
+  strMeasure7: string;
+  strMeasure8: string;
+  strMeasure9: string;
+  strMeasure10: string;
+  strMeasure11: string;
+  strMeasure12: string;
+  strMeasure13: string;
+  strMeasure14: string;
+  strMeasure15: string;
 }
 
 export interface cocktail extends cocktailBase {
-    ingredients:ingredient[];
+  ingredients: ingredient[];
 }
 
 export interface ingredient {
-    name:string;
-    measure:string;
+  name: string;
+  measure: string;
 }
 
-export function convertCocktail(api:cocktailApi):cocktail{
-  const any=api as any;
+export function convertCocktail(api: cocktailApi): cocktail {
+  const any = api as any;
   const ingredients: ingredient[] = [];
-  for (var i = ingredientMinIndex; i <= ingredientMaxIndex; i++)
-    ingredients.push({
-      name: any[`strIngredient${i}`],
-      measure: any[`strMeasure${i}`],
-    });
+  for (var i = ingredientMinIndex; i <= ingredientMaxIndex; i++) {
+    const name = any[`strIngredient${i}`];
+    if (name)
+      ingredients.push({
+        name,
+        measure: any[`strMeasure${i}`],
+      });
+    else break;
+  }
 
   return { ...api, ingredients };
 }
